8a48efefbf08f32165790845c2ea8a5feaaef1a7
Hope Support <help.hope8236@gmail.com>
2025-12-23 11:36:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8280737624-c81b5",
+  "appId": "1:347775247538:web:57983da82df6b9b11655ab",
+  "apiKey": "AIzaSyATLKOC31qm85pcxXGNRDkcvAE4yufOV-Y",
+  "authDomain": "studio-8280737624-c81b5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "347775247538"
+};
